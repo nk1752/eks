84f9c78ee73fdc5355015532a1f1906793b74278
nk1752 <71557302+nk1752@users.noreply.github.com>
2021-09-08 18:32:04 -0500

    sample service

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "nk-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -22,11 +22,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name                 = "education-vpc"
-  cidr                 = "10.0.0.0/16"
+  name                 = "nk-eks-vpc"
+  cidr                 = "10.11.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets      = ["10.11.1.0/24", "10.11.2.0/24", "10.11.3.0/24"]
+  public_subnets       = ["10.11.4.0/24", "10.11.5.0/24", "10.11.6.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
