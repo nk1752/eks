812791c77956c5970aa67d9761727d58887ed9bd
nk1752 <71557302+nk1752@users.noreply.github.com>
2021-09-13 11:52:40 -0500

    changed from my to nk

--- buckets.tf
+++ buckets.tf
@@ -1,8 +1,16 @@
+resource "random_string" "bucket_UUID" {
+  length  = 8
+  special = false
+}
+
+
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "nk-s3-bucket"
+  bucket = "nk-s3-bucket-${lower(random_string.bucket_UUID.result)}"
   acl    = "private"
+
+  force_destroy = true
 
   versioning = {
     enabled = true
